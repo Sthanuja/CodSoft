cdee728cc88329f1d5f1f2504bb3849556146b5b
Sthanuja <129739320+Sthanuja@users.noreply.github.com>
2023-11-23 00:13:34 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,53 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error: Cannot divide by zero."
+
+def Calculator():
+    print("Simple Calculator")
+
+    while True:
+        print("\nOptions:")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Quit")
+
+        choice = input("Enter choice (1/2/3/4/5): ")
+
+        if choice == '5':
+            print("Exiting the calculator. Goodbye!")
+            break
+
+        if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+
+            if choice == '1':
+                result = add(num1, num2)
+                print(f"Result: {num1} + {num2} = {result}")
+            elif choice == '2':
+                result = subtract(num1, num2)
+                print(f"Result: {num1} - {num2} = {result}")
+            elif choice == '3':
+                result = multiply(num1, num2)
+                print(f"Result: {num1} * {num2} = {result}")
+            elif choice == '4':
+                result = divide(num1, num2)
+                print(f"Result: {num1} / {num2} = {result}")
+        else:
+            print("Invalid choice. Please choose a valid operation.")
+
+if __name__ == "__main__":
+    Calculator()
