d846d854f7f14749aaddc4dc73a6fc8be948a51a
Sthanuja <129739320+Sthanuja@users.noreply.github.com>
2023-11-23 11:30:32 +0530

    Update ToDoList.py

--- ToDoList.py
+++ ToDoList.py
@@ -4,59 +4,72 @@ from tkinter import messagebox
 class ToDoList:
     def __init__(self, root):
         self.root = root
-        self.root.title("To-Do List")
+        self.root.title("To-Do List App")
+
         self.tasks = []
 
-        # Create GUI components
+        # Task entry
         self.task_entry = tk.Entry(root, width=40)
-        self.task_entry.pack(pady=10)
+        self.task_entry.grid(row=0, column=0, padx=10, pady=10)
 
-        self.add_button = tk.Button(root, text="Add Task", command=self.add_task)
-        self.add_button.pack()
+        # Buttons
+        add_button = tk.Button(root, text="Add Task", command=self.add_task)
+        add_button.grid(row=0, column=1, padx=10, pady=10)
 
-        self.task_listbox = tk.Listbox(root, selectmode=tk.SINGLE, width=40)
-        self.task_listbox.pack(pady=10)
+        delete_button = tk.Button(root, text="Delete Task", command=self.delete_task)
+        delete_button.grid(row=1, column=0, padx=10, pady=10)
 
-        self.delete_button = tk.Button(root, text="Delete Selected Task", command=self.delete_task)
-        self.delete_button.pack()
+        complete_button = tk.Button(root, text="Mark as Complete", command=self.mark_complete)
+        complete_button.grid(row=1, column=1, padx=10, pady=10)
 
-        self.complete_button = tk.Button(root, text="Mark as Complete", command=self.mark_as_complete)
-        self.complete_button.pack()
+        save_button = tk.Button(root, text="Save", command=self.save_tasks)
+        save_button.grid(row=2, column=0, columnspan=2, pady=10)
 
-        # Populate the listbox with existing tasks
-        self.refresh_listbox()
+        # Task list
+        self.task_listbox = tk.Listbox(root, selectmode=tk.SINGLE, height=10, width=50)
+        self.task_listbox.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+
+        # Load tasks
+        self.load_tasks()
 
     def add_task(self):
         task = self.task_entry.get()
         if task:
             self.tasks.append(task)
-            self.refresh_listbox()
+            self.update_task_list()
             self.task_entry.delete(0, tk.END)
-        else:
-            messagebox.showwarning("Warning", "Please enter a task.")
 
     def delete_task(self):
-        selected_index = self.task_listbox.curselection()
-        if selected_index:
-            self.tasks.pop(selected_index[0])
-            self.refresh_listbox()
-        else:
-            messagebox.showwarning("Warning", "Please select a task to delete.")
+        selected_task_index = self.task_listbox.curselection()
+        if selected_task_index:
+            self.tasks.pop(selected_task_index[0])
+            self.update_task_list()
 
-    def mark_as_complete(self):
-        selected_index = self.task_listbox.curselection()
-        if selected_index:
-            task = self.tasks[selected_index[0]]
-            task = f"{task} (Completed)"
-            self.tasks[selected_index[0]] = task
-            self.refresh_listbox()
-        else:
-            messagebox.showwarning("Warning", "Please select a task to mark as complete.")
+    def mark_complete(self):
+        selected_task_index = self.task_listbox.curselection()
+        if selected_task_index:
+            task = self.tasks[selected_task_index[0]]
+            task = f"[Completed] {task}"
+            self.tasks[selected_task_index[0]] = task
+            self.update_task_list()
 
-    def refresh_listbox(self):
+    def update_task_list(self):
         self.task_listbox.delete(0, tk.END)
         for task in self.tasks:
             self.task_listbox.insert(tk.END, task)
+
+    def save_tasks(self):
+        with open("tasks.txt", "w") as file:
+            for task in self.tasks:
+                file.write(task + "\n")
+
+    def load_tasks(self):
+        try:
+            with open("tasks.txt", "r") as file:
+                self.tasks = [line.strip() for line in file.readlines()]
+                self.update_task_list()
+        except FileNotFoundError:
+            pass
 
 if __name__ == "__main__":
     root = tk.Tk()
